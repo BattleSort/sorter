b5f5547d0f997e920713c5a7bc937a4806ed752d
Keiichiro Seto <shostakovich24@gmail.com>
2019-09-02 21:08:21 +0900

    add penalty

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,5 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   PROBLEM_NUMBER = 3
+  WRONG_ANSWER_PENALTY_SECONDS = 10
   def subscribed
     stream_from room_channel # ルーム内全員
     stream_from user_channel # ユーザーと1on1
@@ -21,15 +22,26 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def submit(hash)
+    # TODO: user_idとか推測されないやつにしないと他のユーザーのフリして回答できちゃう
+    # TODO: token的なのと照合して操作しないと基本的にガバガバ
+
     raise "何かがおかしい" unless REDIS.get(hash["problem_id"])
-    p hash["answer"]
-    p REDIS.get(hash["problem_id"])
+
+    if REDIS.get(user_delay_key).present? 
+      ActionCable.server.broadcast user_channel, message: "10秒以内に再提出はできません", type: "penalty"
+      return
+    end
+
+
     if REDIS.get(hash["problem_id"]) == hash["answer"]
       p "hello ac"
       ActionCable.server.broadcast room_channel, message: "#{hash["user_id"]}さんが#{hash["problem_id"]}を解きました", type: "notice"
+      # 解いた問題の集合、saddで重複されないから連続して送信されたとしても大丈夫
+      REDIS.sadd(hash["user_id"],hash["problem_id"])
     else
       p "hello wa"
       ActionCable.server.broadcast user_channel, message: "不正解です", type: "wrong answer"
+      REDIS.setex(user_delay_key,WRONG_ANSWER_PENALTY_SECONDS,"wrong")
     end
   end
 
@@ -54,6 +66,10 @@ class RoomChannel < ApplicationCable::Channel
 
   def user_channel
     "user-#{params[:user_id]}"
+  end
+
+  def user_delay_key
+    "user-delay-#{params[:user_id]}"
   end
 
   # level, category, count
