238b839b00a9e5665962ba8c1784c9f108273a69
Keiichiro Seto <shostakovich24@gmail.com>
2019-09-13 19:28:11 +0900

    rubocop and annotate

--- app/models/category_ranking.rb
+++ app/models/category_ranking.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: category_rankings
+#
+#  id          :integer          not null, primary key
+#  category_id :integer          not null
+#  ranking_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class CategoryRanking < ApplicationRecord
   belongs_to :ranking
   belongs_to :category
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rankings
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  elements   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Ranking < ApplicationRecord
   has_many :category_rankings, dependent: :destroy
 end
